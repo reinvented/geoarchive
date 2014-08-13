a2c033e69e0f5a95d78a64d6b07656237b1c51b3
Peter Rukavina <peter@rukavina.net>
2014-08-13 12:40:11 -0300

    More tab-cleanup.
    Did I finally find the setting — “Auto expand tabs” — in BBEdit that
    uses spaces for tabs?

--- class.geoarchive.php
+++ class.geoarchive.php
@@ -53,13 +53,13 @@ class GeoArchive
     private function validateArguments()
     {
         if (!date_default_timezone_set($this->time_zone)) {
-                throw new InvalidArgumentException($this->time_zone . ' is not a valid time zone identifier.');
+            throw new InvalidArgumentException($this->time_zone . ' is not a valid time zone identifier.');
         }
 
         if (!$this->input_filename) {
-                throw new InvalidArgumentException('You must specify an input filename or directory.');
+            throw new InvalidArgumentException('You must specify an input filename or directory.');
         } elseif (!file_exists($this->input_filename)) {
-                throw new InvalidArgumentException($this->input_filename . ' does not exist.');
+            throw new InvalidArgumentException($this->input_filename . ' does not exist.');
         }
     }
 
@@ -161,17 +161,17 @@ class GeoArchiveFoursquare extends GeoArchive
     private function parseCheckins()
     {
         foreach ($this->rawevents->Folder->Placemark as $checkin) {
-			$event = array();
-			$event['type'] = 'Feature';
-			list($lon, $lat) = split(',', $checkin->Point->coordinates);
-			$event['geometry'] = array('type' => 'Point', 'coordinates' => array((double) $lon, (double) $lat));
-			$event['properties']['source'] = 'Foursquare';
-			$event['properties']['title'] = (string) $checkin->name;
-			$event['properties']['when'] = strftime('%Y-%m-%d %H:%M:%S +0000', strtotime($checkin->published));
-			list($ID, $comment) = $this->parseFoursquareDescription((string) $checkin->description);
-			$event['properties']['id'] = $ID;
-			$event['properties']['comment'] = $comment;
-			$this->events[] = $event;
+            $event = array();
+            $event['type'] = 'Feature';
+            list($lon, $lat) = split(',', $checkin->Point->coordinates);
+            $event['geometry'] = array('type' => 'Point', 'coordinates' => array((double) $lon, (double) $lat));
+            $event['properties']['source'] = 'Foursquare';
+            $event['properties']['title'] = (string) $checkin->name;
+            $event['properties']['when'] = strftime('%Y-%m-%d %H:%M:%S +0000', strtotime($checkin->published));
+            list($ID, $comment) = $this->parseFoursquareDescription((string) $checkin->description);
+            $event['properties']['id'] = $ID;
+            $event['properties']['comment'] = $comment;
+            $this->events[] = $event;
         }
     }
 
@@ -186,9 +186,9 @@ class GeoArchiveFoursquare extends GeoArchive
     */
     private function parseFoursquareDescription($description)
     {
-		$description = preg_replace("/^@/", '', $description);
-		preg_match("/(.*)\/(.*)\">.*<\/a>-? ?(.*)$/", $description, $matches);
-		return array($matches[2], $matches[3]);
+        $description = preg_replace("/^@/", '', $description);
+        preg_match("/(.*)\/(.*)\">.*<\/a>-? ?(.*)$/", $description, $matches);
+        return array($matches[2], $matches[3]);
     }
 }
 
@@ -259,15 +259,15 @@ class GeoArchivePlazes extends GeoArchive
     private function parseCheckins()
     {
         foreach ($this->rawevents as $activity) {
-			$event = array();
-			$event['type'] = 'Feature';
-			$event['geometry'] = array('type' => 'Point', 'coordinates' => array((double) $this->plazes_id[$activity->activity->plaze_id]->longitude, (double) $this->plazes_id[$activity->activity->plaze_id]->latitude));
-			$event['properties']['source'] = 'Plazes';
-			$event['properties']['title'] = (string) $this->plazes_id[$activity->activity->plaze_id]->name;
-			$event['properties']['when'] = strftime('%Y-%m-%d %H:%M:%S +0000', strtotime($activity->activity->created_at));
-			$event['properties']['id'] = (string) $this->plazes_id[$activity->activity->plaze_id]->id;
-			$event['properties']['comment'] = (string) $activity->activity->status;
-			$this->events[] = $event;
+            $event = array();
+            $event['type'] = 'Feature';
+            $event['geometry'] = array('type' => 'Point', 'coordinates' => array((double) $this->plazes_id[$activity->activity->plaze_id]->longitude, (double) $this->plazes_id[$activity->activity->plaze_id]->latitude));
+            $event['properties']['source'] = 'Plazes';
+            $event['properties']['title'] = (string) $this->plazes_id[$activity->activity->plaze_id]->name;
+            $event['properties']['when'] = strftime('%Y-%m-%d %H:%M:%S +0000', strtotime($activity->activity->created_at));
+            $event['properties']['id'] = (string) $this->plazes_id[$activity->activity->plaze_id]->id;
+            $event['properties']['comment'] = (string) $activity->activity->status;
+            $this->events[] = $event;
         }
     }
 }
@@ -468,13 +468,13 @@ class GeoArchiveOpenpaths extends GeoArchive
     private function parseCheckins()
     {
         foreach ($this->rawevents as $activity) {
-			$event = array();
-			$event['type'] = 'Feature';
-			$event['geometry'] = array('type' => 'Point', 'coordinates' => array((double) $activity->lon, (double) $activity->lat));
-			$event['properties']['source'] = 'Openpaths';
-			$event['properties']['when'] = strftime('%Y-%m-%d %H:%M:%S +0000', $activity->t);
-			$event['properties']['comment'] = (string) $activity->device;
-			$this->events[] = $event;
+            $event = array();
+            $event['type'] = 'Feature';
+            $event['geometry'] = array('type' => 'Point', 'coordinates' => array((double) $activity->lon, (double) $activity->lat));
+            $event['properties']['source'] = 'Openpaths';
+            $event['properties']['when'] = strftime('%Y-%m-%d %H:%M:%S +0000', $activity->t);
+            $event['properties']['comment'] = (string) $activity->device;
+            $this->events[] = $event;
         }
     }
 }
